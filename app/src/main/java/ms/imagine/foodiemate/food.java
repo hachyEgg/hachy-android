9dd7cb1550d1aebddee1d04bc7542ace76769557
Eugene Wang <eugene.r.w.12@gmail.com>
2018-03-27 16:12:52 -0400

    added food interface

---
+++ app/src/main/java/ms/imagine/foodiemate/food.java
@@ -0,0 +1,13 @@
+package ms.imagine.foodiemate;
+
+/**
+ * Created by eugen on 3/27/2018.
+ */
+
+public class food {
+    String name;
+    String location;
+    String Macros;
+    String calories;
+    int score;
+}
