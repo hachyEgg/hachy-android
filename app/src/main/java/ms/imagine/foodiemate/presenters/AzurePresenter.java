d11025fb2858b943768382f2b5e012fa660c118b
Eugene <eugene.r.w.12@gmail.com>
2018-09-18 16:26:03 -0400

    refractor-4-presenter finished

--- app/src/main/java/ms/imagine/foodiemate/presenters/AzurePresenter.java
+++ app/src/main/java/ms/imagine/foodiemate/presenters/AzurePresenter.java
@@ -1,51 +1,54 @@
-package ms.imagine.foodiemate.presenters
+package ms.imagine.foodiemate.presenters;
 
 
 import android.app.Notification;
-import ms.imagine.foodiemate.api.AzureClient
-import ms.imagine.foodiemate.api.AzurePrediction
-import ms.imagine.foodiemate.callbacks.AzureCallBacks
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import android.util.Log;
+import ms.imagine.foodiemate.api.AzureClient;
+import ms.imagine.foodiemate.api.AzurePrediction;
+import ms.imagine.foodiemate.api.Prediction;
+import ms.imagine.foodiemate.callbacks.AzureCallBacks;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.util.HashMap;
+import java.util.List;
 
 public class AzurePresenter implements retrofit2.Callback<AzurePrediction> {
     private AzureClient azureClient;
+    private AzureCallBacks callBacks; 
 
     AzurePresenter(AzureCallBacks callBacks)  {
-        Notification.CarExtender.Builder builder = Retrofit.Builder()
-                .baseUrl(AzureClient.baseURL)
+        this.callBacks = callBacks;
+         Retrofit.Builder builder = new Retrofit.Builder()
+                 .baseUrl(AzureClient.baseURL)
                 .addConverterFactory(GsonConverterFactory.create());
 
-        val retrofit = builder.build()
-        azureClient = retrofit.create(AzureClient::class.java)
-        }
+        Retrofit retrofit = builder.build();
+        azureClient = retrofit.create(AzureClient.class);
+    }
+    
+    public void postImageFromUrl(String remote_url) {
+        azureClient.postImageFromUrl(buildUrl(remote_url)).enqueue(this);
+    }
 
-        fun postImageFromUrl(remote_url: String) {
-        azureClient.postImageFromUrl(buildUrl(remote_url)).enqueue(this)
-        }
+    private HashMap<String, String> buildUrl(String remote_url) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("url", remote_url);
+        return map;
+    }
 
-private fun buildUrl(remote_url: String): HashMap<String, String>{
-        val map = HashMap<String, String>()
-        map.put("url", remote_url)
-        return map
-        }
+    @Override public void onFailure( retrofit2.Call<AzurePrediction> call, Throwable t) {
+        callBacks.onAzureFailure("Cannot Call");
+    }
 
-        override fun onFailure(call: retrofit2.Call<AzurePrediction>?, t: Throwable?) {
-        println("error")
-        println (t.toString())
-        println (call)
-        callBacks.onAzureFailure("Cannot Call")
-        }
-
-        override fun onResponse(call: retrofit2.Call<AzurePrediction>?, response: retrofit2.Response<AzurePrediction>?) {
-        val azurePrediction: AzurePrediction? = response?.body()
-        println("PDC" + response.toString() + azurePrediction)
+    @Override public void onResponse(retrofit2.Call<AzurePrediction> call, retrofit2.Response<AzurePrediction> response) {
+        AzurePrediction azurePrediction = response.body();
+        Log.d("PDC" , response.toString() + azurePrediction);
         if (azurePrediction == null || azurePrediction.predictions == null){
-        callBacks.onAzureFailure(response.toString())
+            callBacks.onAzureFailure(response.toString());
         } else {
-
-        val predictions = azurePrediction.predictions!!
-        callBacks.onAzureSuccess(predictions)
+            List predictions = azurePrediction.predictions;
+            callBacks.onAzureSuccess(predictions);
         }
-        }
-        }
+    }
+}
